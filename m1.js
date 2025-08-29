e1c5f7e529d3a9b3ed588adad24c15374d6f32d3
Lin Xin Wei <77585782+emp1y1@users.noreply.github.com>
2025-08-29 16:09:22 +0800

    Create m1.js

---
+++ m1.js
@@ -0,0 +1,28 @@
+
+
+let body = $response.body;
+try {
+    let obj = JSON.parse(body);
+
+    // 计算当前时间戳 + 20 分钟（秒）
+    let now = Math.floor(Date.now() / 1000) + 7200;
+
+    // 修改 currentTime
+    if (obj?.data?.currentTime) {
+        obj.data.currentTime = now;
+    }
+
+    // 修改 coupon 信息
+    if (obj?.data?.currentGrabCouponInfo?.coupon) {
+        obj.data.currentGrabCouponInfo.coupon.forEach(c => {
+            c.status = 2;
+            c.couponStartTime = now;
+        });
+    }
+
+    body = JSON.stringify(obj);
+} catch (err) {
+    console.log("脚本错误:", err);
+}
+
+$done({ body });
